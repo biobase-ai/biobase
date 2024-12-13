0cadbcc29a3d70292f962c42b62e3f2a61ff183c
johnson.chen <joy6677@qq.com>
2024-12-13 17:30:27 +0800

    fix

--- apps/design-system/contentlayer.config.js
+++ apps/design-system/contentlayer.config.js
@@ -6,10 +6,10 @@ import rehypePrettyCode from 'rehype-pretty-code'
 import rehypeSlug from 'rehype-slug'
 import { codeImport } from 'remark-code-import'
 import remarkGfm from 'remark-gfm'
+import remarkFrontmatter from 'remark-frontmatter'
 import { visit } from 'unist-util-visit'
 
 import { rehypeComponent } from './lib/rehype-component'
-import { rehypeNpmCommand } from './lib/rehype-npm-command'
 
 /** @type {import('contentlayer2/source-files').ComputedFields} */
 const computedFields = {
@@ -23,40 +23,7 @@ const computedFields = {
   },
 }
 
-const LinksProperties = defineNestedType(() => ({
-  name: 'LinksProperties',
-  fields: {
-    doc: {
-      type: 'string',
-    },
-    api: {
-      type: 'string',
-    },
-  },
-}))
-
-const NestedProperties = defineNestedType(() => ({
-  name: 'NestedProperties',
-  fields: {
-    radix: {
-      type: 'boolean',
-    },
-    shadcn: {
-      type: 'boolean',
-    },
-    vaul: {
-      type: 'boolean',
-    },
-    inputOtp: {
-      type: 'boolean',
-    },
-    reactAccessibleTreeview: {
-      type: 'boolean',
-    },
-  },
-}))
-
-export const Doc = defineDocumentType(() => ({
+const Doc = defineDocumentType(() => ({
   name: 'Doc',
   filePathPattern: `docs/**/*.mdx`,
   contentType: 'mdx',
@@ -73,132 +40,41 @@ export const Doc = defineDocumentType(() => ({
       type: 'boolean',
       default: true,
     },
-    links: {
-      type: 'nested',
-      of: LinksProperties,
-    },
-    featured: {
-      type: 'boolean',
-      default: false,
-      required: false,
-    },
     component: {
       type: 'boolean',
       default: false,
       required: false,
     },
-    fragment: {
-      type: 'boolean',
-      default: false,
-      required: false,
-    },
-    toc: {
-      type: 'boolean',
-      default: true,
-      required: false,
-    },
     source: {
-      type: 'nested',
-      of: NestedProperties,
+      type: 'json',
+      required: false,
     },
   },
   computedFields,
 }))
 
+const theme = {
+  dark: 'github-dark',
+  light: 'github-light',
+}
+
 export default makeSource({
   contentDirPath: './content',
   documentTypes: [Doc],
   mdx: {
-    remarkPlugins: [remarkGfm, codeImport],
+    remarkPlugins: [remarkGfm, codeImport, remarkFrontmatter],
     rehypePlugins: [
       rehypeSlug,
       rehypeComponent,
-      () => (tree) => {
-        visit(tree, (node) => {
-          if (node?.type === 'element' && node?.tagName === 'pre') {
-            const [codeEl] = node.children
-            if (codeEl.tagName !== 'code') {
-              return
-            }
-
-            if (codeEl.data?.meta) {
-              // Extract event from meta and pass it down the tree.
-              const regex = /event="([^"]*)"/
-              const match = codeEl.data?.meta.match(regex)
-              if (match) {
-                node.__event__ = match ? match[1] : null
-                codeEl.data.meta = codeEl.data.meta.replace(regex, '')
-              }
-            }
-
-            node.__rawString__ = codeEl.children?.[0].value
-            node.__src__ = node.properties?.__src__
-            node.__style__ = node.properties?.__style__
-          }
-        })
-      },
-      [
-        rehypePrettyCode,
-        // rehypePrettyCodeOptions,
-        {
-          getHighlighter: async () => {
-            const theme = await loadTheme(path.join(process.cwd(), '/lib/themes/biobase-2.json'))
-            return await getHighlighter({ theme })
-          },
-          onVisitLine(node) {
-            // Prevent lines from collapsing in `display: grid` mode, and allow empty
-            // lines to be copy/pasted
-            if (node.children.length === 0) {
-              node.children = [{ type: 'text', value: ' ' }]
-            }
-          },
-          onVisitHighlightedLine(node) {
-            node.properties.className.push('line--highlighted')
-          },
-          onVisitHighlightedWord(node) {
-            node.properties.className = ['word--highlighted']
-          },
-        },
-      ],
-      () => (tree) => {
-        visit(tree, (node) => {
-          if (node?.type === 'element' && node?.tagName === 'div') {
-            if (!('data-rehype-pretty-code-fragment' in node.properties)) {
-              return
-            }
-
-            const preElement = node.children.at(-1)
-            if (preElement.tagName !== 'pre') {
-              return
-            }
-
-            preElement.properties['__withMeta__'] = node.children.at(0).tagName === 'div'
-            preElement.properties['__rawString__'] = node.__rawString__
-
-            if (node.__src__) {
-              preElement.properties['__src__'] = node.__src__
-            }
-
-            if (node.__event__) {
-              preElement.properties['__event__'] = node.__event__
-            }
-
-            if (node.__style__) {
-              preElement.properties['__style__'] = node.__style__
-            }
-          }
-        })
-      },
-      // rehypeNpmCommand,
       [
         rehypeAutolinkHeadings,
         {
-          properties: {
-            className: ['subheading-anchor'],
-            ariaLabel: 'Link to section',
-          },
+          behavior: 'append',
+          test: ['h2', 'h3', 'h4'],
+          properties: { className: ['subheading-anchor'] },
         },
       ],
+      [rehypePrettyCode, { theme }],
     ],
   },
 })
